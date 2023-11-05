3a144802b996fd8954422736a4229fca2bf9beab
ouamefi <the-freestyler@outlook.com>
2023-11-05 14:30:26 +0100

    Cleanup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Client } = require('discord.js');
 const client = new Client({ intents: 2048 });
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const fs = require('fs');
+const keepAlive = require("./server/server.js");
 
 const ACCOUNTS_FILE = './accounts.json';
 
@@ -36,23 +37,24 @@ async function sendMessage(username, tweetId) {
 async function fetchTweets() {
   let accounts = loadObjectsFromJsonFile(ACCOUNTS_FILE);
   for(acc of accounts) {
-    console.log(`Fetching Tweets of @${acc.username}...`);
+    //console.log(`Fetching Tweets of @${acc.username}...`);
     url = `https://syndication.twitter.com/srv/timeline-profile/screen-name/${acc.username}`
     const opts = { headers: { cookie: env.USER_COOKIE } };
 
     const response = await fetch(url, opts);
     const responseHTML = await response.text();
-    const jsonData = responseHTML.substring(responseHTML.indexOf('__NEXT_DATA__')+39, responseHTML.indexOf('}</script>')+1);
+    const jsonData = responseHTML.substring(responseHTML.indexOf('__NEXT_DATA__')+39, responseHTML.indexOf('}</script>')+1); //use bs4
     const tweets = JSON.parse(jsonData).props.pageProps.timeline.entries;
-    console.log('Fetched and parsed Tweets!');
+    //console.log('Fetched and parsed Tweets!');
 
     let lastTweetId = acc.last_tweet_id;
     let newLastTweetId = lastTweetId;
 
+    // Only process the last 5 tweets
     for(let i = 0; i < 5; i++) {
       let tweetId = tweets[i].entry_id.substring(6);
       if(tweetId !== lastTweetId) {
-        //Latest post
+        // Save latest tweet id
         if (i == 0) newLastTweetId = tweetId;
         sendMessage(acc.username, tweetId);
       } else {
@@ -63,7 +65,7 @@ async function fetchTweets() {
     acc.last_tweet_id = newLastTweetId;
   }
   writeObjectsToJsonFile(ACCOUNTS_FILE, accounts);
-  console.log('Accounts file updated!');
+  //console.log('Accounts file updated!');
 }
 
 client.login(process.env.DISCORD_TOKEN)
